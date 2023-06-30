f9132c200ff81bc288b742ed3714f51aea38cffe
Nahuelement <nahuel.perugi@gmail.com>
2023-06-29 21:04:56 -0400

    models comentarios

--- app1/admin.py
+++ app1/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Plato
+from .models import Plato, Comentarios
 
 # Register your models here.
 
 admin.site.register(Plato)
+admin.site.register(Comentarios)
 
 
--- app1/models.py
+++ app1/models.py
@@ -3,10 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Plato(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre_plato = models.CharField(max_length=50)
     descripcion = models.TextField(max_length=200)
     img = models.CharField(max_length=50)
     precio = models.IntegerField()
     def __str__(self):
         return self.nombre
 
+class Comentarios(models.Model):
+    nombre_cliente = models.CharField(max_length=50)
+    opinion = models.TextField(max_length=200)
+    nota = models.IntegerField()
+    def __str__(self):
+        return self.nombre
