9c5036bdc23b42b4115f9c9961815a40c5a6a0f2
Jorge Sanzana <jorge.sanzana94@gmail.com>
2023-07-04 20:25:04 -0400

    Eventos

--- app1/admin.py
+++ app1/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Plato, Comentarios, Reservas
+from .models import Plato, Comentarios, Reservas, Evento
 
 # Register your models here.
 
 admin.site.register(Plato)
 admin.site.register(Comentarios)
 admin.site.register(Reservas)
-
+admin.site.register(Evento)
 
